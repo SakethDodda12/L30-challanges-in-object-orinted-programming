fb605baaf3c54a9cb0156e11eb51974ae7d510bc
SakethDodda12 <sakethdodda57@gmail.com>
2025-11-07 20:32:10 -0500

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,28 @@
+import random
+
+class fruitquiz:
+    def __init__(self):
+        self.fruits = {'apple':'red',
+                       'orange':'orange',
+                       'watermelon':'green',
+                       'banana' : 'yellow'}
+    def quiz(self):
+        while(True):
+
+            fruit, color = random.choice(list(self.fruits.items()))
+
+            print("what is the color of {}".format(fruit))
+            user_answer = input()
+
+            if(user_answer.lower() == color):
+                print("Corret answer")
+            else:
+                print("wrong answer")
+            
+            option = int(input("enter 0, if you want to play again otherwise enter 1:"))
+            if(option):
+                break
+
+print("Welcome to fruitquiz")
+fq = fruitquiz()
+fq.quiz
